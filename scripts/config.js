ac18a111a32f971f1ec001735297c3df6bb0df31
Rafal Konstanty <raf.konstanty@gmail.com>
2021-11-06 21:20:54 +0100

    Created Player edit feature along with storing playerName data

---
+++ scripts/config.js
@@ -0,0 +1,36 @@
+function openPlayerConfig(event) {
+    editedPlayer = +event.target.dataset.playerid;
+    playerConfigOverlayElement.style.display = 'block';
+    backdropElement.style.display = 'block';
+}
+
+function closePlayerConfig(){
+    playerConfigOverlayElement.style.display = 'none';
+    backdropElement.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error');
+    errorsOutputElement.textContent='';
+    formElement.firstElementChild.lastElementChild.value = '';
+}
+
+function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayerName = formData.get('username').trim();
+    if (!enteredPlayerName) {
+        event.target.firstElementChild.classList.add('error');
+        errorsOutputElement.textContent = 'Please enter a valid name!';
+        return; //
+    }
+
+    const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
+    updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+
+    // if (editedPlayer === 1) {
+    //     player[0].name =enteredPlayerName;
+    // }else {
+    //     player[1].name = enteredPlayerName;
+    // }
+    player[editedPlayer -1].name = enteredPlayerName;
+    
+    closePlayerConfig();
+}
